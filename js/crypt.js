8f23743c950288e00ef193454f467853509061b3
Rafał Michalski <royaltm75@gmail.com>
2015-08-22 19:41:50 +0200

    crypt: sync and async en(de)cryptor

--- js/crypt.js
+++ js/crypt.js
@@ -21,10 +21,14 @@ exports.loadOrCreateKeyIv = loadOrCreateKeyIv;
 exports.createEncryptorDecryptor = createEncryptorDecryptor;
 
 /**
- * Create pair encryptor and decryptor functions using key loaded from entropy file.
- * If entropy file doesn't exitst creates new entropy.
+ * Create encryptor and decryptor functions using key loaded from entropy file.
  *
- * @param {string} filePath - a path to entropy file
+ * If entropy file doesn't exist creates new entropy and writes it to a file.
+ * Makes sure that the file is created only once, by using exclusive file flag.
+ *
+ * If no `filePath` is given, creates new entropy without saving it.
+ *
+ * @param {string} [filePath] - an optional path to entropy file
  * @param {string|Buffer} [password] - optional: pass null to create password
  * @param {string|Buffer} [salt] - optional: pass null to create salt
  * @param {Function} callback - function(err, encryptor, decryptor)
@@ -38,11 +42,11 @@ exports.loadOrCreateEncryptorDecryptor = function(filePath, password, salt, call
 
 function createEncryptorDecryptor(cipherKey, iv, hmacKey, callback) {
   function encryptor(data, callback) {
-    encrypt(cipherKey, iv, hmacKey, data, callback);
+    return encrypt(cipherKey, iv, hmacKey, data, callback);
   }
 
   function decryptor(dataEncrypted, callback) {
-    decrypt(cipherKey, iv, hmacKey, dataEncrypted, callback);
+    return decrypt(cipherKey, iv, hmacKey, dataEncrypted, callback);
   }
 
   if (callback)
@@ -52,30 +56,39 @@ function createEncryptorDecryptor(cipherKey, iv, hmacKey, callback) {
 };
 
 function loadOrCreateKeyIv(filePath, password, salt, callback) {
+  if (!filePath)
+    return createEntropyKeyIvHmac(password, salt, callback);
+
   var retries = 10;
   (function readEntropy(err) {
 
     if (err && retries-- <= 0)
       return callback(err);
 
+    /* read entropy file */
     fs.readFile(filePath, function(err, entropy) {
-      if (err && err.code !== 'ENOENT') {
+      if (err && err.code !== 'ENOENT')
         return callback(err);
-      }
+
       if (entropy) {
+        /* file exists */
         if (entropy.length === 80) {
-          extractKeyIv(entropy, callback);
+          /* the size is right, resolve it */
+          extractKeyIvHmac(entropy, callback);
         } else if (entropy.length === 0) {
+          /* file is empty, some other process might be writing to it */
           setTimeout(readEntropy, 50, new Error("entropy file found but it's empty"));
         } else
           callback(new Error("entropy file has incorrect size"));
       } else {
+        /* create file exclusively, create entropy, write entropy to a file */
         fs.open(filePath, 'wx', function(err, fd) {
           if (err)
-            return err.code === 'EEXIST' ? setTimeout(readEntropy, 50, err) : callback(err);
+            return err.code === 'EEXIST' ?
+                setTimeout(readEntropy, 50, err) : callback(err);
 
+          /* clean up file on error */
           function cleanup(err) {
-            var args = arguments;
             fs.close(fd, function() {
               fs.unlink(filePath, function() {
                 callback(err);
@@ -83,14 +96,16 @@ function loadOrCreateKeyIv(filePath, password, salt, callback) {
             });
           }
 
+          /* create entropy */
           createEntropy(password, salt, function(err, entropy) {
             if (err) return cleanup(err);
+            /* write entropy to a file */
             fs.write(fd, entropy, 0, entropy.length, function(err, written) {
               if (err) return cleanup(err);
               if (written != entropy.length)
                 return cleanup(new Error("error writing entropy file"));
               fs.close(fd, function() {
-                extractKeyIv(entropy, callback);
+                extractKeyIvHmac(entropy, callback);
               });
             });
           });
@@ -100,6 +115,13 @@ function loadOrCreateKeyIv(filePath, password, salt, callback) {
   })();
 };
 
+function createEntropyKeyIvHmac(password, salt, callback) {
+  createEntropy(password, salt, function(err, entropy) {
+    if (err) return callback(err);
+    extractKeyIvHmac(entropy, callback);
+  });
+}
+
 function createEntropy(password, salt, callback) {
   password || (password = crypto.randomBytes(6));
   salt     || (salt     = crypto.randomBytes(64));
@@ -107,7 +129,7 @@ function createEntropy(password, salt, callback) {
   crypto.pbkdf2(password, salt, config.entropyIterations, 80, config.entropyHmacAlgorithm, callback);
 }
 
-function extractKeyIv(entropy, callback) {
+function extractKeyIvHmac(entropy, callback) {
   var iv        = entropy.slice(0, 16)   // [  0:128]
     , cipherKey = entropy.slice(16, 48)  // [128:384]
     , hmacKey   = entropy.slice(48, 80); // [384:640]
@@ -121,18 +143,31 @@ function encrypt(cipherKey, iv, hmacKey, data, algorithm, callback) {
   algorithm || (algorithm = config.cryptoAlgorithm);
 
   try {
-    var cipher = crypto.createCipheriv(algorithm, cipherKey, iv);
+    var cipher = crypto.createCipheriv(algorithm, cipherKey, iv)
+      , signer = crypto.createHmac(config.signatureHmacAlgorithm, hmacKey)
+  } catch(err) {
+    if (callback) return callback(err);
+    throw err;
+  }
+
+  if (callback) {
     cipher.end(data, function() {
       try {
-        var cipherbody = cipher.read()
-          , authbody = crypto.createHmac(config.signatureHmacAlgorithm, hmacKey).update(cipherbody).digest();
+        var cipherbody = cipher.read();
+        signer.end(cipherbody, function() {
+          var authbody = signer.read();
+          callback(null, Buffer.concat([authbody, cipherbody]));
+        });
       } catch(err) { return callback(err); }
-
-      callback(null, Buffer.concat([authbody, cipherbody]));
     });
-  } catch(err) { return callback(err); }
+  } else {
+    var cipherbody = cipher.update(data)
+      , cipherpad = cipher.final()
+      , authbody = signer.update(cipherbody).update(cipherpad).digest()
+    return Buffer.concat([authbody, cipherbody, cipherpad]);
+  }
 };
-
+// TODO make it synchronous also
 function decrypt(cipherKey, iv, hmacKey, dataEncrypted, algorithm, callback) {
   if ('function' === typeof algorithm)
     callback = algorithm, algorithm = null;
@@ -142,19 +177,36 @@ function decrypt(cipherKey, iv, hmacKey, dataEncrypted, algorithm, callback) {
   try {
     var authbody = dataEncrypted.slice(0, 32)
       , cipherbody = dataEncrypted.slice(32)
-      , verify = crypto.createHmac(config.signatureHmacAlgorithm, hmacKey).update(cipherbody).digest()
-      , decipher = crypto.createDecipheriv(algorithm, cipherKey, iv);
+      , decipher = crypto.createDecipheriv(algorithm, cipherKey, iv)
+      , signer = crypto.createHmac(config.signatureHmacAlgorithm, hmacKey)
 
+  } catch(err) {
+    if (callback) return callback(err);
+    throw err;
+  }
+
+  if (callback) {
+    signer.end(cipherbody, function() {
+      var verify = signer.read();
+      if (timingSafeComparison(verify, authbody)) {
+        decipher.end(cipherbody, function() {
+          try {
+            var data = decipher.read();
+          } catch(err) { return callback(err); }
+          callback(null, data);
+        });
+      } else
+        callback(null, null);
+    });
+  } else {
+    var verify = signer.update(cipherbody).digest();
     if (timingSafeComparison(verify, authbody)) {
-      decipher.end(cipherbody, function() {
-        try {
-          var data = decipher.read();
-        } catch(err) { return callback(err); }
-        callback(null, data);
-      });
+      var datapart = decipher.update(cipherbody)
+        , datapad = decipher.final();
+      return Buffer.concat([datapart, datapad]);
     } else
-      callback(null, null);
-  } catch(err) { return callback(err); }
+      return null;
+  }
 };
 
 function timingSafeComparison(buf1, buf2) {
--- test/js/test.crypt.js
+++ test/js/test.crypt.js
@@ -19,34 +19,54 @@ var testdata = {
 }
 
 test("should create entropy file and encryptor, decryptor pair", function(t) {
-  t.plan(11);
+  t.plan(17);
   t.type(crypt.loadOrCreateEncryptorDecryptor, 'function');
   crypt.loadOrCreateEncryptorDecryptor(entropyFile, null, null, function(err, enc, dec) {
     t.ifErr(err);
     t.type(enc, 'function');
     t.type(dec, 'function');
+    
+    var encdata = enc(testdata.plain);
+    t.type(encdata, Buffer);
+    t.notDeepEqual(encdata, testdata.plain);
+    t.notEqual(encdata.length, testdata.plain.length);
+    var vrfydata = dec(encdata);
+    t.type(vrfydata, Buffer);
+    t.deepEqual(vrfydata, testdata.plain);
+    testdata.encrypted = encdata;
+
     enc(testdata.plain, function(err, encdata) {
       t.ifErr(err);
       t.type(encdata, Buffer);
       t.notDeepEqual(encdata, testdata.plain);
       t.notEqual(encdata.length, testdata.plain.length);
+      t.deepEqual(encdata, testdata.encrypted);
       dec(encdata, function(err, vrfydata) {
         t.ifErr(err);
         t.type(vrfydata, Buffer);
         t.deepEqual(vrfydata, testdata.plain);
-        testdata.encrypted = encdata;
       });
     })
   });
 });
 
 test("should create encryptor, decryptor pair from entropy file", function(t) {
-  t.plan(13);
+  t.plan(19);
   t.type(crypt.loadOrCreateEncryptorDecryptor, 'function');
   crypt.loadOrCreateEncryptorDecryptor(entropyFile, null, null, function(err, enc, dec) {
     t.ifErr(err);
     t.type(enc, 'function');
     t.type(dec, 'function');
+
+    var encdata = enc(testdata.plain);
+    t.type(encdata, Buffer);
+    t.notDeepEqual(encdata, testdata.plain);
+    t.notEqual(encdata.length, testdata.plain.length);
+    t.deepEqual(encdata, testdata.encrypted);
+    var vrfydata = dec(encdata);
+    t.type(vrfydata, Buffer);
+    t.deepEqual(vrfydata, testdata.plain);
+
     enc(testdata.plain, function(err, encdata) {
       t.ifErr(err);
       t.type(encdata, Buffer);
